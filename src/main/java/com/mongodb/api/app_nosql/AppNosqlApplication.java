380531d00ffd8995b4eb152c06beb6d77fa8f3d0
joao <jrodrigo887@gmail.com>
2023-06-27 23:08:58 -0300

    refactor: reconfigurando o docker

--- src/main/java/com/mongodb/api/app_nosql/AppNosqlApplication.java
+++ src/main/java/com/mongodb/api/app_nosql/AppNosqlApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppNosqlApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AppNosqlApplication.class, args);
 	}
-
 }
