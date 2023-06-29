76b7cdf39007222a51c7e5e36ffcde6604715a98
joao <jrodrigo887@gmail.com>
2023-06-28 21:43:48 -0300

    fix: correção no nome da collection

--- src/main/java/com/mongodb/api/app_nosql/domain/User.java
+++ src/main/java/com/mongodb/api/app_nosql/domain/User.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "users")
+@Document
 public class User implements Serializable {
     @Id
     private String id;
