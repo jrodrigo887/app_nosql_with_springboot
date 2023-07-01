89b6206341f343ad49cb17476084338543e66ce8
joao <jrodrigo887@gmail.com>
2023-06-30 22:35:27 -0300

    feat: atualização de usuário

--- src/main/java/com/mongodb/api/app_nosql/resources/UserResource.java
+++ src/main/java/com/mongodb/api/app_nosql/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,6 +52,14 @@ public class UserResource {
                 .buildAndExpand(userDto.getId()).toUri();
 
         return ResponseEntity.created(uri).body(UserMapper.toDto(result));
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> update(@RequestBody UserDTO userDto, @PathVariable String id) {
+        User user = UserMapper.fromDTO(userDto);
+        user.setId(id);
+        service.update(user);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping(value = "/{id}")
--- src/main/java/com/mongodb/api/app_nosql/services/UserService.java
+++ src/main/java/com/mongodb/api/app_nosql/services/UserService.java
@@ -3,6 +3,7 @@ package com.mongodb.api.app_nosql.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,5 +35,15 @@ public class UserService {
     public void delete(String id) {
         findById(id);
         repo.deleteById(id);
+    }
+
+    public User update(User user) {
+        User entitUser = findById(user.getId());
+        return repo.save(updateData(entitUser, user));
+    }
+
+    private User updateData(User entiUser, User obj) {
+            BeanUtils.copyProperties(obj, entiUser);
+            return entiUser;
     }
 }
