1a66872516b7d2bb20beaf3acc0689173560c6e2
joao <jrodrigo887@gmail.com>
2023-07-01 22:00:42 -0300

    styles: configurando o seed para post

--- src/main/java/com/mongodb/api/app_nosql/config/InstanceConfig.java
+++ src/main/java/com/mongodb/api/app_nosql/config/InstanceConfig.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.mongodb.api.app_nosql.domain.Post;
 import com.mongodb.api.app_nosql.domain.User;
+import com.mongodb.api.app_nosql.repository.PostRepository;
 import com.mongodb.api.app_nosql.repository.UserRepository;
 
 @Configuration
@@ -16,17 +18,25 @@ public class InstanceConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository repository;
+    
+    @Autowired
+    private PostRepository postRepository;
 
     @Override
     public void run(String... args) throws Exception {
         repository.deleteAll();
+        postRepository.deleteAll();
         var us1 = new User(null, "João", "joao@email.com");
         var us2 = new User(null, "Silva", "silva@email.com");
         var us3 = new User(null, "Filipe", "filipe@email.com");
         var us4 = new User(null, "Maria", "mariape@email.com");
         var us5 = new User(null, "Aline", "aline@email.com");
         var us6 = new User(null, "Lua", "lua@email.com");
+        Post post1 = new Post(null, "Correntes marítimas", "Para entender melhor...", us1);
+        Post post3 = new Post(null, "Dados quebrado", "Para entender melhor...", us1);
+
         repository.saveAll(Arrays.asList(us1, us2, us3, us4, us5, us6));
+        postRepository.saveAll(Arrays.asList(post1, post3));
     }
     
 }
