bb1756708ab4314bd83c3e4aaa7757eeeba25c47
joao <jrodrigo887@gmail.com>
2023-07-05 21:49:00 -0300

    style: exemplo de query raw

--- src/main/java/com/mongodb/api/app_nosql/repository/PostRepository.java
+++ src/main/java/com/mongodb/api/app_nosql/repository/PostRepository.java
@@ -3,9 +3,16 @@ package com.mongodb.api.app_nosql.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.mongodb.api.app_nosql.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+    // exemplo de query method
     List<Post> findByTitleContaining(String text);
+
+    // exemplo de query raw mongoDB
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
 }
